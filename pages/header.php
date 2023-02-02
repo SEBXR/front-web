73742ab0b83278ef49e615a8b54fc2bb36d90f20
SEBXR <99262672+SEBXR@users.noreply.github.com>
2023-02-02 03:38:09 +0300

    CALME TOI ETIENNE

--- pages/header.php
+++ pages/header.php
@@ -4,13 +4,13 @@
         if(isset($_SESSION["id"])){
             $names = fetchUrl(link."getNames/".$_SESSION["id"]);
             print "<h3>Bienvenue $names</h3>";
-            print "<a href='../index.php'><i  class='fas fa-list'></i>Liste des enchères</a>";
+            print "<a href='listeEncheres.php'><i  class='fas fa-list'></i>Liste des enchères</a>";
             print "<a href='historique.php'><i class='fas fa-user'></i>Historique</a>";
             print "<a href='login.php?destroy'><i class='fas fa-user'></i>Se déconnecter</a>";
         }
         else{
             $action = link."recherchetous";
-            print "<a href='../index.php'><i  class='fas fa-list'></i>Liste des enchères</a>";
+            print "<a href='listeEncheres.php'><i  class='fas fa-list'></i>Liste des enchères</a>";
             print "<a href='login.php'><i class='fas fa-user'></i>Se connecter</a>";
         }
     ?>
